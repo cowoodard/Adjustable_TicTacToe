62a2d993f1796941bd712f4da6bf8a4e05089909
cowoodard <49789212+cowoodard@users.noreply.github.com>
2021-08-16 08:44:09 -0500

    Update main.cpp
    The initial 3x3 standard TicTacToe board was change to add dynamic board sizes.
    Updates include: User selectable board size input and changes to the winner checking function (boardStatus) to all for accurate checking for a winner of any size board.

--- main.cpp
+++ main.cpp
@@ -1,48 +1,108 @@
 #include <iostream>
+#include <iomanip>
 #include <string>
+#include <vector>
 
 using namespace std;
 
-void boardGraphic (string displayBoard[]){
+void boardGraphic (vector<string>& displayBoard, int& printWidth){
 
-    cout << displayBoard[0] << " | " << displayBoard[1] << " | " << displayBoard[2] << endl;
-    cout << "---------" << endl;
-    cout << displayBoard[3] << " | " << displayBoard[4] << " | " << displayBoard[5] << endl;
-    cout << "---------" << endl;
-    cout << displayBoard[6] << " | " << displayBoard[7] << " | " << displayBoard[8] << endl;
-
+    for (int i = 0; i <= printWidth - 1; i++){
+        cout << "|";
+        for (int j = 0; j <= printWidth - 1; j++){
+            cout << setw(3) << displayBoard[(i * printWidth) + j] << " | ";
+        }
+        cout << endl;
+        for (int k = 0; k <= printWidth - 1; k++){
+            cout << "------";
+        }
+        cout<< endl;
+    }
 }
 
 //This function tests the board matrix for a winner
-bool boardStatus (string showBoard[]){
+bool boardStatus (vector<string>& showBoard, int& boardWidth){
 
-    int i = 0;
-    if (showBoard[0] == showBoard[1] && showBoard[1] == showBoard[2]){
-        return true;
+    bool winnerFound = false;
+    bool checkComplete = false;
+
+    while (winnerFound == false && checkComplete == false){
+            //Checking for a winner on the horizontals
+            for (int i = 0; i <= boardWidth - 1; i++){
+                if (winnerFound == true){
+                    break;
+                }
+                for (int j = 0; j <= boardWidth - 2; j++){
+                    if ((showBoard[(i * boardWidth) + j] != showBoard[(i * boardWidth) + (j + 1)])){
+                        break;
+                    }
+                    if ((showBoard[(i * boardWidth) + j] == showBoard[(i * boardWidth) + (j + 1)]) && (j == boardWidth - 2)){
+                        winnerFound = true;
+                        break;
+                    }
+                    else {}
+                }
+            }
+            if (winnerFound == true){
+                break;
+            }
+            //Checking for a winner on the verticals
+            for (int k = 0; k <= boardWidth - 1; k++){
+                for (int y = 1; y <= boardWidth - 1; y++){
+                    if (winnerFound == true){
+                        break;
+                    }
+                    if (showBoard[k] != showBoard[(y * boardWidth) + k]){
+                        break;
+                    }
+                    if (showBoard[k] == showBoard[(y * boardWidth) + k] && (y == boardWidth - 1)){
+                        winnerFound = true;
+                        break;
+                    }
+                    else{}
+                }
+            }
+            if (winnerFound == true){
+                break;
+            }
+            //Checking for a winner on the diagonals, board index positions 0-4-8 & 6-4-2 in a 3x3 for reference
+            int q = 0; //Settings comparison for vector index 0 to check downward diagonal
+            for (int w = 1; w <= boardWidth - 1; w++){
+                cout << "First Diagonal " << showBoard[q] << " " <<showBoard[(w * boardWidth) + w] << endl;
+                if (winnerFound == true){
+                    break;
+                }
+                if (showBoard[q] != showBoard[(w * boardWidth) + w]){
+                    break;
+                }
+                if (showBoard[q] == showBoard[(w * boardWidth) + w] && (w == boardWidth - 1)){
+                    winnerFound = true;
+                }
+                else{}
+            }
+            if (winnerFound == true){
+                break;
+            }
+            q = boardWidth - 1; //Settings comparison for vector index boardWidth to check upward diagonal
+            for (int r = 1; r <= boardWidth - 1; r++){
+                cout << "Second Diagonal " << showBoard[q] << " " <<showBoard[q + (r * boardWidth) - 1] << endl;
+                if (winnerFound == true){
+                    break;
+                }
+                if (showBoard[q] != showBoard[q + (r * boardWidth) - 1]){
+                    break;
+                }
+                if (showBoard[q] == showBoard[q + (r * boardWidth) - 1] && (r == boardWidth - 1)){
+                    winnerFound == true;
+                }
+                else{}
+            }
+            if (winnerFound == true){
+                break;
+            }
+            checkComplete = true; //Exit if all possible winning positions have been checked
     }
-    else if (showBoard[3] == showBoard[4] && showBoard[4] == showBoard[5]){
-        return true;
-    }
-    else if (showBoard[6] == showBoard[7] && showBoard[7] == showBoard[8]){
-        return true;
-    }
-    else if (showBoard[0] == showBoard[3] && showBoard[3] == showBoard[6]){
-        return true;
-    }
-    else if (showBoard[1] == showBoard[4] && showBoard[4] == showBoard[7]){
-        return true;
-    }
-    else if (showBoard[2] == showBoard[5] && showBoard[5] == showBoard[8]){
-        return true;
-    }
-    else if (showBoard[0] == showBoard[4] && showBoard[4] == showBoard[8]){
-        return true;
-    }
-    else if (showBoard[2] == showBoard[4] && showBoard[4] == showBoard[6]){
-        return true;
-    }
-    else
-        return false;
+    return winnerFound;
 }
 
 
@@ -52,13 +112,13 @@ int main()
     //Variable to indicate status of game initialized to false at beginning
     bool winner = false;
     //These are the letter indicators assigned to each player and the standard size board (later to add selected nxn sized board determined by input
-    string player1, player2, board[9];
-    int selection;
+    string player1, player2, board;
+    int selection, currentPlayer, boardSize, boardNSize;
 
     //Populates board with options
-    for (int i = 0; i <= (sizeof(board)/sizeof(board[0]))-1; i++){
-        board[i] = to_string(i + 1);
-    }
+//    for (int i = 0; i <= (sizeof(board)/sizeof(board[0]))-1; i++){
+//        board[i] = to_string(i + 1);
+//    }
 
     cout << "Welcome to TicTacToe" << endl;
     cout << "Would you like to play? (Type Yes or No)"<< endl;
@@ -76,40 +136,51 @@ int main()
             player2 = "X";
         }
 
+        cout << "Please chose board dimension (N) for an NxN board" << endl;
+        cin >> boardNSize;
+        boardSize = boardNSize * boardNSize;
+        vector<string> board;
+
+        for (int i = 0; i < boardSize; i++){
+            board.push_back(to_string(i+1));
+        }
+
         cout << "Player 1 will play as " << player1 << endl;
         cout << "Player 2 will play as " << player2 << endl;
         cout << "Lets begin" << endl;
         // boardStatus (boardPtr) call function to display current board passing pointers of player
-        boardGraphic (board);
+        boardGraphic (board, boardNSize);
         int numTurns = 0;
+        currentPlayer = 1;
         //While loop where the game is played that exits when a winner is declared.
         while (winner != true){
-            cout << "Player 1, Please select a number position you would like to place a marker" << endl;
+
+            cout << "Player " << currentPlayer << ", Please select a number position you would like to place a marker" << endl;
             cin >> selection;
-            board[selection-1] = player1;
-            boardGraphic (board);
-            winner = boardStatus(board);
+            if (currentPlayer == 1){
+                board[selection-1] = player1;
+                //board
+            } else {
+                board[selection-1] = player2;
+            }
+            boardGraphic (board, boardNSize);
+            winner = boardStatus(board, boardNSize);
             if (winner == true){
-                cout << "Congratulations Player 1 has won the game" << endl;
+                cout << "Congratulations Player " << currentPlayer << " has won the game" << endl;
                 break;
             }
             else{
                 numTurns++;
             }
-            cout << "Player 2, Please select a number position you would like to place a marker" << endl;
-            cin >> selection;
-            board[selection-1] = player2;
-            boardGraphic (board);
-            if (winner == true){
-                cout << "Congratulations Player 2 has won the game" << endl;
-                break;
-            }
-            else{
-                numTurns++;
-            }
-            if (numTurns == 9){
+            if (numTurns == board.size()){
                 cout << "The game is a tie" << endl;
                 break;
+            }
+            if (currentPlayer == 1){
+                currentPlayer = 2;
+            }
+            else {
+                currentPlayer = 1;
             }
         }
         cout << "Play again soon" << endl;
